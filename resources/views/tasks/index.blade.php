2839f5aa31dafb089e3b6a3eade3d30181b4fdf2
kaisei0201 <kaisei0201ok@gmail.com>
2021-09-25 11:03:53 +0900

    タスク一覧表示機能作成 ステータス分け未実装

--- resources/views/tasks/index.blade.php
+++ resources/views/tasks/index.blade.php
@@ -27,7 +27,7 @@
                             </a>
                         </div>
                         <div class="list-group">
-                            @foreach($folders as $folder)
+                            @foreach ($folders as $folder)
                                 <a href="{{ route('tasks.index', ['id' => $folder->id]) }}"
                                     class="list-group-item {{ $current_folder_id === $folder->id ? 'active' : '' }}">
                                     {{ $folder->title }}
@@ -37,7 +37,41 @@
                     </nav>
                 </div>
                 <div class="column col-md-8">
-                    <!-- ここにタスクが表示される -->
+                    <div class="panel panel-default">
+                        <div class="panel-heading">
+
+                            <div class="panel-body">
+                                <div class="text-right">
+                                    <a href="#" class="btn btn-default btn-block">
+                                        {{-- タスクを追加する --}}
+                                    </a>
+                                </div>
+                            </div>
+
+                            <table class="table">
+                                <thead>
+                                    <tr>
+                                        <th>タイトル</th>
+                                        <th>状態</th>
+                                        <th>期限</th>
+                                        <th></th>
+                                    </tr>
+                                </thead>
+                                <tbody>
+                                    @foreach ($tasks as $task)
+                                        <tr>
+                                            <td>{{ $task->title }}</td>
+                                            <td>
+                                                <span class="label">{{ $task->status }}</span>
+                                            </td>
+                                            <td>{{ $task->due_date }}</td>
+                                            <td><a href="#">編集</a></td>
+                                        </tr>
+                                    @endforeach
+                                </tbody>
+                            </table>
+                        </div>
+                    </div>
                 </div>
             </div>
         </div>
